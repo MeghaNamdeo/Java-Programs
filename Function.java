9f3d122020466273e8d0f1c2cb73fceb9ed61c96
MeghaNamdeo <96281329+MeghaNamdeo@users.noreply.github.com>
2023-12-27 17:55:55 +0530

    Create Function.java

---
+++ Function.java
@@ -0,0 +1,20 @@
+class Function {
+    static void fun1() {
+        System.out.println("A");
+    }
+
+    static void fun2() {
+        System.out.println("B");
+    }
+
+    static void fun3() {
+        System.out.println("C");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        fun1();
+        fun2();
+        fun3();
+    }
+}
